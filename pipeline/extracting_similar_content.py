daa9e0a0b1af5d3ddb34f882415a9fc7f9897026
Raghava Kamuju <raghavakamuju@gmail.com>
2025-04-27 14:42:14 +0530

    feat: update top k value for data retreival

--- pipeline/extracting_similar_content.py
+++ pipeline/extracting_similar_content.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sentence_transformers import SentenceTransformer
 
-def find_similar_content(user_query, faiss_index, text_extracted_list, model_name="sentence-transformers/all-MiniLM-L6-v2", k=3):
+def find_similar_content(user_query, faiss_index, text_extracted_list, model_name="sentence-transformers/all-MiniLM-L6-v2", k=5):
     """Finds and returns the top-k similar content for a user query."""
     model = SentenceTransformer(model_name)
     query_embedding = model.encode(user_query).astype('float32')
